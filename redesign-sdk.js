deeb2ade634ce7b8d616b329ad5f42b870b6183a
Jan Kristian Valgijainen <54046100+ogchibi95@users.noreply.github.com>
2025-03-26 11:17:18 +0200

    Create redesign-sdk.js

---
+++ redesign-sdk.js
@@ -0,0 +1,18 @@
+/**
+ * My own personal, Redesign SDK wrapper file.
+ *
+ **/
+(function (window) {
+
+    // declare
+    var redesignSDK = {};
+
+    // your sdk init function
+    redesignSDK.init = function () {
+        // ...
+    };
+
+    // define your namespace myApp
+    window.redesignSDK = redesignSDK;
+
+})(window, undefined);
